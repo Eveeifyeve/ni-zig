1b7ebc66900c3369d6e5ec6903d79ab45b460588
eveeifyeve <eveeg1971@gmail.com>
2024-08-03 20:04:52 +1000

    feat: project init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          lib,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devenv.shells.default = {
+            dotenv.enable = false;
+            packages = lib.optionals pkgs.stdenv.isDarwin (
+              with pkgs;
+              [
+                darwin.apple_sdk.frameworks.Security
+                darwin.apple_sdk.frameworks.SystemConfiguration
+              ]
+            );
+            languages.zig = {
+              enable = true;
+            };
+          };
+        };
+    };
+}
